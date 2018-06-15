1e23cf5458c462bf956ff5b44c3a81752b529e3f
Akash Goyal <goyalakash391@gmail.com>
2018-06-15 11:21:11 +0530

    completed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,27 @@
-#write your code here
+def echo word
+	word
+end
+def shout word
+	word.upcase
+end
+def repeat word, num = 2
+	((word + " ")*(num - 1)) + word 
+end
+def start_of_word word, num
+	word[0,num]
+end
+def first_word string
+	string.split[0]
+end
+def titleize string
+	omit = ["and","if","the","for","over"]
+	words = string.split
+	words.each do |i|
+		if omit.include?(i) == false
+			i.capitalize!
+		end
+	end
+	words = words.join(" ")
+	words[0] = words[0].upcase
+	words
+end
