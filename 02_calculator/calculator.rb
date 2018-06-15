704b51c49cfc369e26b61f1dc11320aec8b0835c
Akash Goyal <goyalakash391@gmail.com>
2018-06-15 11:22:53 +0530

    completed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,22 @@
-#write your code here
+def add a, b 
+	a+b
+end
+
+def subtract a, b
+	a-b
+end
+
+def sum a
+	sum = 0
+	a.length.times do |i|
+		sum = sum + a[i]
+	end
+	sum
+end
+def multiply a,b
+	a*b
+end
+def fractorial
+end
+def power
+end	
