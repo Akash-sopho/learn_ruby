af246304f0fb52b146fd4ed9acc9d71c88b930cd
Akash_Goyal <goyalakash391@gmail.com>
2018-06-19 00:21:04 +0530

    completed

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+  attr_accessor :title
+  def title
+    exceptions = ['and','in','the','of','a','an']
+    words = @title.split
+    words.length.times do |i|
+      if exceptions.include?(words[i]) == false
+        words[i] = words[i].capitalize
+      end
+    end
+    @title = words.join(" ")
+    @title[0] = @title[0].upcase
+    @title
+  end
 end
