290abcc2e8d5174cb3313a91f7110f3b226d4579
Akash_Goyal <goyalakash391@gmail.com>
2018-06-19 00:07:50 +0530

    completed

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,12 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def seconds
+    @seconds = 0
+  end
+  def time_string
+    hours= @seconds/3600
+    minutes=(@seconds%3600)/60
+    second=(@seconds%3600)%60
+    return "%02d" %hours+":"+"%02d" %minutes+":"+"%02d" %second
+  end
 end
