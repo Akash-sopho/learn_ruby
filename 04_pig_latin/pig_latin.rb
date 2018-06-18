a3b423db9db2a4faf5f734c8d5f434180d19e6a9
Akash_Goyal <goyalakash391@gmail.com>
2018-06-18 21:19:53 +0530

    completed

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,18 @@
-#write your code here
+def translate string
+  vowels = ['a','e','i','o'];
+  words = string.split
+  if words.length == 1
+    if (vowels.include?(string[0]))
+      string = string + "ay"
+    else
+      string = string[1,string.length - 1] + string[0]
+      string = translate(string)
+    end
+  else
+    words.length.times do |i|
+      words[i] = translate(words[i])
+    end
+    string = words.join(" ")
+  end
+  string
+end
